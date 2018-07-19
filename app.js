38d36de11a764aa94fdba5f97305e81f9736589e
ryanjtan <41416264+ryanjtan@users.noreply.github.com>
2018-07-20 02:43:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ const cameraView = document.querySelector("#camera--view"),
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
-        .then(function (stream) {
+        .then(function(stream) {
         track = stream.getTracks()[0];
         cameraView.srcObject = stream;
-    });
+    })
     .catch(function(error) {
         console.error("Oops. Something is broken.", error);
     });
-};
+}
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
